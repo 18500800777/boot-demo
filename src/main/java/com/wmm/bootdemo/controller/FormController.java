65c0adfd2cd29be310d5bafec908734eaa3a49bd
wangmingming <lhwmm0204@163.com>
2022-06-02 14:35:43 +0800

    hot-fix second commit

--- src/main/java/com/wmm/bootdemo/controller/FormController.java
+++ src/main/java/com/wmm/bootdemo/controller/FormController.java
@@ -20,6 +20,8 @@ public class FormController {
         // int i= 10/0;
         System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
+        System.out.println("7");
         return "form/form_layouts";
     }
 
